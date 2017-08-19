a6533adef291c150a06906fc19e49d80dd9a5b34
Andrii Marynets <andrii.marynets@gmail.com>
2017-08-19 17:54:04 +0300

    Add test for create item in buffer

--- test/test_buffer.py
+++ test/test_buffer.py
@@ -1,6 +1,6 @@
 import unittest
 
-from app import Buffer
+from app import Buffer, Item, Resource, Cost
 
 
 class BufferTestCase(unittest.TestCase):
@@ -11,3 +11,10 @@ class BufferTestCase(unittest.TestCase):
         item_before = 'v1:3414:16590:119073:6af64456-8d69-4427-9c2e-32a97fe50ae3'
         item_after = ['3414', '16590', '119073', '6af64456-8d69-4427-9c2e-32a97fe50ae3']
         self.assertEqual(self.buffer._split(item_before), item_after, 'Item spliting is bad')
+
+    def test_create_item(self):
+        in_buffer = Item('3411', Resource(0, 'env'), Cost(0.0045, 0))
+        self.buffer._create(in_buffer.id, in_buffer.resource, in_buffer.cost)
+
+        self.assertEqual(self.buffer.get('3411env'), in_buffer, 'Not equals')
+        self.assertEqual(self.buffer.get('6af64456-8d69-4427-9c2e-32a97fe50ae3'), None, 'Not equals')
