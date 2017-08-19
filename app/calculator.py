502679604b53a85223f5694248fa141fa3b460ca
Andrii Marynets <andrii.marynets@gmail.com>
2017-08-19 16:50:10 +0300

    Add functional for add item

--- app/calculator.py
+++ app/calculator.py
@@ -1,9 +1,22 @@
 from collections import namedtuple
 
 Cost = namedtuple('Cost', 'current, in_db')
-Type = namedtuple('Type', 'id, name')
-Item = namedtuple('Item', 'id, type, cost')
+Resource = namedtuple('Resource', 'id, name')
+Item = namedtuple('Item', 'id, resource, cost')
 
 
+class Buffer:
+    def __init__(self):
+        self.buffer = dict()
+        self.resource = [Resource(i, j) for i, j in enumerate(['env', 'farm', 'farm_role', 'server'])]
 
+    def add(self, item):
+        items = item['user:scalr-meta'].split(':')[1:]
+        cost = item['Cost']
+        for i, j in zip(items, self.resource):
+            record = self.buffer.get(i + j.name)
+            if record:
+                pass
+            else:
+                self._create(i + j.name, j, cost)
 
