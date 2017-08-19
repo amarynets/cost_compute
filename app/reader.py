e34049a7b9d3bd6f8236f2a681067110d9db7a1b
Andrii Marynets <andrii.marynets@gmail.com>
2017-08-19 15:03:52 +0300

    Add function for select data by func criterion

--- app/reader.py
+++ app/reader.py
@@ -1,5 +1,15 @@
+import csv
 
 
 class Reader:
     def __init__(self, filename):
         self.name = filename
+
+    def get_data(self, criterion):
+        with open(self.name, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for i in reader:
+                if criterion(i):
+                    yield i
+                else:
+                    continue
