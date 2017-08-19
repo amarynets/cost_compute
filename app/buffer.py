fc970d5f8c6f574bf0953c02197e424f4145e5c6
Andrii Marynets <andrii.marynets@gmail.com>
2017-08-19 17:02:38 +0300

    Add split item function

--- app/buffer.py
+++ app/buffer.py
@@ -11,7 +11,7 @@ class Buffer:
         self.resource = [Resource(i, j) for i, j in enumerate(['env', 'farm', 'farm_role', 'server'])]
 
     def add(self, item):
-        items = item['user:scalr-meta'].split(':')[1:]
+        items = self._split(item)
         cost = item['Cost']
         for i, j in zip(items, self.resource):
             record = self.buffer.get(i + j.name)
@@ -26,3 +26,6 @@ class Buffer:
     def _update(self, item, cost):
         item.cost.current += cost
         self.buffer[item.name] = item
+
+    def _split(self, item):
+        return item['user:scalr-meta'].split(':')[1:]
