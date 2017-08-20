a69fec0c5a59e54bb9140f728e0f5fd8a8ef6d45
Andrii Marynets <andrii.marynets@gmail.com>
2017-08-20 16:23:51 +0300

    Add write many record to db

--- app/writer.py
+++ app/writer.py
@@ -1,14 +1,13 @@
-from app import Database
-
 
 class Writer:
-    def __init__(self, name):
-        self.db = Database(name)
+    def __init__(self, database):
+        self.db = database
 
     def write(self, item):
         try:
             if isinstance(item, list):
-                pass
+                self.db.insert_many('''INSERT INTO cost(object_type, object_id, cost) VALUES(?, ?, ?)''',
+                                    (item))
             else:
                 return self.db.insert('''INSERT INTO cost(object_type, object_id, cost) VALUES(?, ?, ?)''',
                                       (item.resource.id, item.id, item.cost.current))
--- test/test_writer.py
+++ test/test_writer.py
@@ -1,13 +1,13 @@
 import unittest
 
-from app import Writer, Buffer
+from app import Writer, Buffer, Database
 from . import create_db
 
 
 class WriterTestCase(unittest.TestCase):
     def setUp(self):
         self.buffer = Buffer()
-        self.writer = Writer(':memory:')
+        self.writer = Writer(Database(':memory:'))
         self.writer.db = create_db(self.writer.db)
         self.buffer.add({'Cost': 0.0045, 'user:scalr-meta': 'v1:3414:16590:119073:6af64456-8d69-4427-9c2e-32a97fe50ae3'})
         self.buffer.add({'Cost': 0.01, 'user:scalr-meta': 'v1:32:3453:22342:34dfrg56-8d69-4427-9c2e-32a97fe50ae3'})
