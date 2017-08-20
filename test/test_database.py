be0de723c8e08706bbeb3c024fb77c6f0114215d
Andrii Marynets <andrii.marynets@gmail.com>
2017-08-20 14:23:08 +0300

    Add test to DB

---
+++ test/test_database.py
@@ -0,0 +1,14 @@
+import unittest
+
+from app import Database
+
+from . import create_db
+
+
+class DBTestCase(unittest.TestCase):
+    def setUp(self):
+        self.db = create_db(Database(':memory:'))
+
+    def test_insert_one(self):
+        r = self.db.insert('''INSERT INTO cost(object_type, object_id, cost) VALUES(?, ?, ?)''', (0, '3414', 0.55))
+        self.assertEqual(r, 1, 'Not correct id')
