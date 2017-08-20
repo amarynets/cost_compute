a3661e1e7d0856feafdf9d2b2ad0301f047d7966
Andrii Marynets <andrii.marynets@gmail.com>
2017-08-20 21:54:47 +0300

    Update function for split meta field
    Because any() with '' not work correctly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 from sys import getsizeof
 
-from app import Database, Reader, Buffer, Writer
+from app import Database, Reader, Buffer, Writer, Scanner
 from const import *
 
 db = Database('scalr.db')
@@ -8,20 +9,22 @@ db.create_table(CREATE_ENUM_TABLE)
 db.create_table(CREATE_COST_TABLE)
 db.insert_many(INSERT_ENUM_DATA, ENUM_DATA)
 
-reader = Reader('aws0.csv')
 
 def f(row):
     meta = row['user:scalr-meta']
     meta = meta.split(':')[1:]
-    if any(len(i) > 0 for i in meta):
-        return True
-    else:
-        return False
-count = 0
+    count = 0
+    for i in meta:
+        if len(i) > 0:
+            count += 1
+    return True if count == 4 else False
+reader = Reader(Scanner('/home/andrii/study/tt/cost_compute').get_files())
 buf = Buffer()
 writer = Writer(db)
 for i in reader.get_data(f):
     buf.add(i)
+
+print(buf.get('1farm'))
 
 for i in buf.get_buffer(100):
     writer.write(i)
