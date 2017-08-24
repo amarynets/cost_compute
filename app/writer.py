7032f3aa38fba127964644eef7b2e44fdfe270b5
Andrii Marynets <andrii.marynets@gmail.com>
2017-08-24 22:40:01 +0300

    Add buffer for check if item exits in db

--- app/writer.py
+++ app/writer.py
@@ -2,6 +2,7 @@
 class Writer:
     def __init__(self, database):
         self.db = database
+        self.type = {k: list() for k in range(4)}
 
     def write(self, item):
         try:
@@ -13,3 +14,9 @@ class Writer:
                                       (item.resource.id, item.id, item.cost))
         except Exception as e:
             print(e)
+
+    def _is_in_db(self, item):
+        if item[1] in self.type[0]:
+            return True
+        else:
+            return False
